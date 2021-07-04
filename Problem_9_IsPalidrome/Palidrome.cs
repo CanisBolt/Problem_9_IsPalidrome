82e17ea07444eb52a52ff79bda08e868b73af54a
CanisBolt <canisbolt@gmail.com>
2021-07-04 04:18:59 +0300

    First Commit

--- Problem_9_IsPalidrome/Palidrome.cs
+++ Problem_9_IsPalidrome/Palidrome.cs
@@ -8,7 +8,8 @@ namespace Problem_9_IsPalidrome
 {
     class Palidrome
     {
-        /*
+        /* 
+         * LeetCode Problem #9
          * Given an integer x, return true if x is palindrome integer.
          * An integer is a palindrome when it reads the same backward as forward. 
          * For example, 121 is palindrome while 123 is not.
